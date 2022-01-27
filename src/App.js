4cff741ab1d844aa383d81cd1e616b943035d4c3
Akash-Ramjyothi <akash.ramjyothi@kivihealth.com>
2022-01-27 22:07:57 +0530

    Added Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import GetList from './components/GetList';
 
 function App() {
+
+  const [studentList, setStudentList] = useState([]);
+  useEffect(()=>{
+    async function fetchStudentList(){
+      try{
+        const requestUrl = 'https://61f2c7672219930017f508c6.mockapi.io/api/student';
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        console.log(responseJSON);
+        setStudentList(responseJSON);
+      } catch {
+
+      }
+    }
+    fetchStudentList();
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <GetList studentList={studentList}/>
   );
 }
 
